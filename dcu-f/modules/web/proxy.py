9031900f2b463edd2f32f89494c1b10aa01bf1c7
Alexander Krupenkin <alexandr.krupenkin@gmail.com>
2012-09-18 16:53:43 +0400

    web: user interface

--- dcu-f/modules/web/proxy.py
+++ dcu-f/modules/web/proxy.py
@@ -1,2 +1,17 @@
+from flask import render_template, request
+from dcu import Client
+
+
+def draw_client_list():
+    clist = Client.query.all()
+    return render_template('web_client_list.html', clist=clist)
+
+make_html = {
+    'client_list': draw_client_list,
+}
+
 def run(client):
-    return "Hello, I`m DCU-Web!!!"
+    if 'get' in request.values:
+        req = request.values['get']
+        return make_html[req]()
+    return render_template('web_base.html', client=client)
