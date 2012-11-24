be9769a7f6d09ffe573629a843147484b8a90069
Alexander Krupenkin <alexandr.krupenkin@gmail.com>
2012-11-24 23:54:37 +0400

    ship: servo motor control

--- modules/ship/proxy.py
+++ modules/ship/proxy.py
@@ -10,12 +10,14 @@ class SHIP(db.Model):
     m_front = db.Column(db.Integer())
     m_middle = db.Column(db.Integer())
     m_back = db.Column(db.Integer())
+    m_servo = db.Column(db.Integer())
 
     def __init__(s, uid):
         s.uid = uid
         s.m_front = 127
         s.m_middle = 127
         s.m_back = 127
+        s.m_servo = 127
 
 def run(client):
     if 'get' in request.values:
@@ -23,7 +25,7 @@ def run(client):
         if req == 'motors':
             ship = SHIP.query.filter_by(uid=client.uid).first()
             if ship:
-                return dumps((127, ship.m_front, ship.m_middle, ship.m_back))
+                return dumps((ship.m_servo, ship.m_front, ship.m_back, ship.m_middle))
             else:
                 c = SHIP(client.uid)
                 db.session.add(c)
@@ -36,8 +38,10 @@ def run(client):
             ship.m_front = request.values['speed']
         elif request.values['motor'] == 'middle':
             ship.m_middle = request.values['speed']
-        else:
+        elif request.values['motor'] == 'back':
             ship.m_back = request.values['speed']
+        else:
+            ship.m_servo = request.values['speed']
 
         db.session.add(ship)
         db.session.commit()
